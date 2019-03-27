fde2fd2d4698475cf190439cdff4686001860288
Philipp Ossler <philipp.ossler@gmail.com>
2019-03-27 14:44:50 +0100

    update get-started guide
    * upgrade to Zeebe 0.16.4
    * remove variable mappings
    * remove custom headers

--- src/main/java/io/zeebe/Application.java
+++ src/main/java/io/zeebe/Application.java
@@ -1,94 +1,86 @@
 package io.zeebe;
 
-import java.util.*;
-
 import io.zeebe.client.ZeebeClient;
 import io.zeebe.client.api.events.DeploymentEvent;
 import io.zeebe.client.api.events.WorkflowInstanceEvent;
 import io.zeebe.client.api.subscription.JobWorker;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
 
-public class Application
-{
+public class Application {
 
-    public static void main(String[] args)
-    {
-        final String contactPoint = args.length >= 1 ? args[0] : "127.0.0.1:26500";
+  public static void main(String[] args) {
+    final String contactPoint = args.length >= 1 ? args[0] : "127.0.0.1:26500";
 
-        System.out.println("Connecting to broker: " + contactPoint);
+    System.out.println("Connecting to broker: " + contactPoint);
 
-        final ZeebeClient client = ZeebeClient.newClientBuilder()
-            .brokerContactPoint(contactPoint)
-            .build();
+    final ZeebeClient client =
+        ZeebeClient.newClientBuilder().brokerContactPoint(contactPoint).build();
 
-        System.out.println("Connected to broker: " + contactPoint);
+    System.out.println("Connected to broker: " + contactPoint);
 
-        final DeploymentEvent deployment = client.newDeployCommand()
-            .addResourceFromClasspath("order-process.bpmn")
-            .send()
-            .join();
+    final DeploymentEvent deployment =
+        client.newDeployCommand().addResourceFromClasspath("order-process.bpmn").send().join();
 
-        final int version = deployment.getWorkflows().get(0).getVersion();
-        System.out.println("Workflow deployed. Version: " + version);
+    final int version = deployment.getWorkflows().get(0).getVersion();
+    System.out.println("Workflow deployed. Version: " + version);
 
-        final Map<String, Object> data = new HashMap<>();
-        data.put("orderId", 31243);
-        data.put("orderItems", Arrays.asList(435, 182, 376));
+    final Map<String, Object> data = new HashMap<>();
+    data.put("orderId", 31243);
+    data.put("orderItems", Arrays.asList(435, 182, 376));
 
-        final WorkflowInstanceEvent wfInstance = client.newCreateInstanceCommand()
+    final WorkflowInstanceEvent wfInstance =
+        client
+            .newCreateInstanceCommand()
             .bpmnProcessId("order-process")
             .latestVersion()
             .variables(data)
             .send()
             .join();
 
-        final long workflowInstanceKey = wfInstance.getWorkflowInstanceKey();
+    final long workflowInstanceKey = wfInstance.getWorkflowInstanceKey();
 
-        System.out.println("Workflow instance created. Key: " + workflowInstanceKey);
+    System.out.println("Workflow instance created. Key: " + workflowInstanceKey);
 
-        final JobWorker jobWorker = client
+    final JobWorker jobWorker =
+        client
             .newWorker()
             .jobType("payment-service")
-            .handler((jobClient, job) ->
-            {
-                final Map<String, Object> headers = job.getCustomHeaders();
-                final String method = (String) headers.get("method");
+            .handler(
+                (jobClient, job) -> {
+                  final Map<String, Object> variables = job.getPayloadAsMap();
 
-                final Map<String, Object> payload = job.getPayloadAsMap();
+                  System.out.println("Process order: " + variables.get("orderId"));
+                  System.out.println("Collect money");
 
-                System.out.println("Process order: " + payload.get("orderId"));
-                System.out.println("Collect money using payment method: " + method);
+                  // ...
 
-                // ...
+                  final Map<String, Object> result = new HashMap<>();
+                  result.put("totalPrice", 46.50);
 
-                payload.put("totalPrice", 46.50);
-                jobClient.newCompleteCommand(job.getKey())
-                    .payload(payload)
-                    .send()
-                    .join();
-            })
+                  jobClient.newCompleteCommand(job.getKey()).payload(result).send().join();
+                })
+            .fetchVariables("orderId")
             .open();
 
-        waitUntilClose();
+    waitUntilClose();
 
-        jobWorker.close();
+    jobWorker.close();
 
-        client.close();
-        System.out.println("Closed.");
-    }
+    client.close();
+    System.out.println("Closed.");
+  }
 
-    private static void waitUntilClose()
-    {
-        try (Scanner scanner = new Scanner(System.in))
-        {
-            while (scanner.hasNextLine())
-            {
-                final String nextLine = scanner.nextLine();
-                if (nextLine.contains("close"))
-                {
-                    return;
-                }
-            }
+  private static void waitUntilClose() {
+    try (Scanner scanner = new Scanner(System.in)) {
+      while (scanner.hasNextLine()) {
+        final String nextLine = scanner.nextLine();
+        if (nextLine.contains("close")) {
+          return;
         }
+      }
     }
-
+  }
 }
